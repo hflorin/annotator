c53b3f5947030ec0260ee123eefc41b309fe4338
hflorin <florinel.hociung@yahoo.com>
2016-03-23 23:40:53 +0200

    show content

---
+++ Code/SemanticRelationsResolver.Annotator/Wrapper/Custom/DocumentWrapper.cs
@@ -0,0 +1,13 @@
+﻿namespace SemanticRelationsResolver.Annotator.Wrapper
+{
+    using System.Linq;
+
+    public partial class DocumentWrapper
+    {
+        public string IdValue
+        {
+            get { return Attributes.Single(a => a.Name.Equals("id")).Value; }
+            set { Attributes.Single(a => a.Name.Equals("id")).Value = value; }
+        }
+    }
+}
--- Code/SemanticRelationsResolver.Annotator/Wrapper/Custom/SentenceWrapper.cs
+++ Code/SemanticRelationsResolver.Annotator/Wrapper/Custom/SentenceWrapper.cs
@@ -6,6 +6,36 @@
 
     public partial class SentenceWrapper
     {
+        public string IdValue
+        {
+            get { return Attributes.Single(a => a.Name.Equals("id")).Value; }
+            set { Attributes.Single(a => a.Name.Equals("id")).Value = value; }
+        }
+
+        public string ContentValue
+        {
+            get { return Attributes.Single(a => a.Name.Equals("content")).Value; }
+            set { Attributes.Single(a => a.Name.Equals("content")).Value = value; }
+        }
+
+        public string ParserValue
+        {
+            get { return Attributes.Single(a => a.Name.Equals("parser")).Value; }
+            set { Attributes.Single(a => a.Name.Equals("parser")).Value = value; }
+        }
+
+        public string UserValue
+        {
+            get { return Attributes.Single(a => a.Name.Equals("user")).Value; }
+            set { Attributes.Single(a => a.Name.Equals("user")).Value = value; }
+        }
+
+        public string DateValue
+        {
+            get { return Attributes.Single(a => a.Name.Equals("date")).Value; }
+            set { Attributes.Single(a => a.Name.Equals("date")).Value = value; }
+        }
+
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (Attributes == null)
@@ -15,7 +45,7 @@
 
             if (string.IsNullOrWhiteSpace(Attributes.Single(a => a.Name.Equals("parser")).Value))
             {
-                yield return new ValidationResult("Parser is required.", new[] { "Parser" });
+                yield return new ValidationResult("Parser is required.", new[] {"Parser"});
             }
         }
     }
