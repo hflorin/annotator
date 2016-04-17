a259270639828e565dedb5867b44eb8161f8f8af
flori <flori@DESKTOP-5DSEIO8>
2016-04-17 19:28:53 +0300

    setup to display non-editable attributes in readonly textbox and attributes with predefined set of values in combobox

---
+++ Code/SemanticRelationsResolver.Annotator/Wrapper/Custom/AttributeWrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace SemanticRelationsResolver.Annotator.Wrapper
+{
+    using System.Linq;
+
+    public partial class AttributeWrapper
+    {
+        public bool HasAllowedValuesSet
+        {
+            get { return AllowedValuesSet.Any(); }
+        }
+    }
+}
