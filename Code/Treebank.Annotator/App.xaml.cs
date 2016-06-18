cae80c9788149f2adf388859b425f6a8ed5baa32
florinel.hociuung@yahoo.com <flori@DESKTOP-5DSEIO8>
2016-06-18 09:21:53 +0300

    add logging on application wide errors

--- Code/Treebank.Annotator/App.xaml.cs
+++ Code/Treebank.Annotator/App.xaml.cs
@@ -1,28 +1,31 @@
 ﻿namespace Treebank.Annotator
 {
+    using System;
     using System.Windows;
     using System.Windows.Controls;
-
     using Autofac;
-
+    using NLog;
     using Prism.Events;
-
-    using Treebank.Annotator.Startup;
-    using Treebank.Annotator.View;
-    using Treebank.Annotator.ViewModels;
+    using Startup;
+    using View;
+    using ViewModels;
 
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += MyHandler;
+
             base.OnStartup(e);
 
             // set tooltip to show indefinetly
             ToolTipService.ShowDurationProperty.OverrideMetadata(
-                typeof(DependencyObject), 
+                typeof(DependencyObject),
                 new FrameworkPropertyMetadata(int.MaxValue));
 
-            ToolTipService.InitialShowDelayProperty.OverrideMetadata(typeof(DependencyObject), new FrameworkPropertyMetadata(10));
+            ToolTipService.InitialShowDelayProperty.OverrideMetadata(typeof(DependencyObject),
+                new FrameworkPropertyMetadata(10));
 
             var bootstrapper = new Bootstrapper();
 
@@ -33,6 +36,20 @@
             MainWindow = new MainWindow(viewModel, container.Resolve<IEventAggregator>());
 
             MainWindow.Show();
+        }
+
+        private void MyHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            var logger = LogManager.GetCurrentClassLogger(typeof(App));
+            logger.Error(e.ExceptionObject);
+
+            MessageBox.Show("An unexpectected error occured! See logs for more details.");
+            var mainViewModel = MainWindow.DataContext as MainViewModel;
+
+            if (mainViewModel != null)
+            {
+                mainViewModel.OnClosing(null);
+            }
         }
     }
 }
